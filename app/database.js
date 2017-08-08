7d6dd6ee324cee0caebe6f41ba3a5a73c3fb1730
Naveen Mannam <naveen.mannam@hotmail.com>
2017-08-08 14:23:58 -0400

    Added lokijs custom database

---
+++ app/database.js
@@ -0,0 +1,21 @@
+var loki = require('lokijs');
+
+var db = new loki('db.json');
+
+db
+    .addCollection('top')
+    .insert([
+        { term: 'Javascript', style: 'warning' },
+        { term: 'Angular 2', style: 'warning' },
+        { term: 'NodeJS', style: 'warning' },
+        { term: 'ios', style: 'warning' },
+        { term: 'ReactJS', style: 'warning' },
+        { term: 'Ionic', style: 'warning' },
+        { term: 'REST', style: 'warning' },
+        { term: 'Authentication', style: 'warning' },
+        { term: 'Android', style: 'warning' }
+    ]);
+
+db.addCollection('searches');
+
+db.saveDatabase();
